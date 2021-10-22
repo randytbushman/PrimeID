944670e2bfe76d43f57c0e5a80234a145a6e403c
randytbushman <bushmanr1@students.rowan.edu>
2021-10-22 11:23:22 -0400

    Added single threaded factor finder methods

--- src/singlethreadedfactorizer/FactorFinder.java
+++ src/singlethreadedfactorizer/FactorFinder.java
@@ -6,7 +6,26 @@ import java.util.List;
 
 public class FactorFinder
 {
-    public Map<Integer, List<Integer>> findFactors(int n) {
-        return null;
+    private Map<Integer, List<Integer>> factorMap;
+
+    public FactorFinder() {
+        factorMap = new HashMap<>();
     }
+
+    /**
+     * This method locates the factors of a given int n then stores the result in the factor map.
+     * @param n the int whose factors we find.
+     * @return
+     */
+    public void findFactors(int n) {
+
+    }
+
+    /**
+     * This method prints the current state of the factor map.
+     */
+    public void printMap() {
+        System.out.println(factorMap);
+    }
+
 }
