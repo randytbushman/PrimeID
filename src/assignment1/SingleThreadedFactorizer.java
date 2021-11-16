f5978e168504f432c855a68ce27631045b3cfd6f
randytbushman <bushmanr1@students.rowan.edu>
2021-11-16 13:54:57 -0500

    Overloaded factorize method in single threaded mode that has a starting and ending number.

--- src/assignment1/SingleThreadedFactorizer.java
+++ src/assignment1/SingleThreadedFactorizer.java
@@ -17,7 +17,7 @@ public class SingleThreadedFactorizer
      * Given an int, n, this method locates the prime numbers between 2 and n. The numbers that are not prime
      * will be stored in a map whose keys represent the composite numbers and whose values represent a list of
      * that number's factors. The time it takes to complete shall then be printed.
-     * @param n calculate and store the prime numbers between 2 and n inclusively. n > 2
+     * @param n calculate and store the prime numbers between 2 and n inclusively. n > 2.
      */
     public static void factorize(int n) {
         long startTime = System.nanoTime();
@@ -41,4 +41,50 @@ public class SingleThreadedFactorizer
         //System.out.println(primeList);
         //System.out.println(factorMap+"\n\n");
     }
+
+
+    /**
+     * Given an int, start, and int, end this method locates the prime numbers between start and end inclusively. The
+     * numbers that are not prime will be stored in a map whose keys represent the composite numbers and whose values
+     * represent a list of that number's factors. The time it takes to complete shall then be printed.
+     * @param start calculate and store the prime numbers between start and end inclusively. start < end.
+     * @param end calculate and store the prime numbers between start and end inclusively. end > 2.
+     */
+    public static void factorize(int start, int end) {
+        long startTime = System.nanoTime();
+        List<Integer> primeList = new ArrayList<>();
+        Map<Integer, List<Integer>> factorMap = new HashMap<>();
+
+        if (start == 2)
+            primeList.add(2);
+        if (start <= 3)
+            primeList.add(3);
+
+        int factorStart;
+        int primeStart;
+        if (start % 2 == 0) {
+            factorStart = start;
+            primeStart = start + 1;
+        }
+        else {
+            factorStart = start + 1;
+            primeStart = start;
+        }
+
+
+        // All even number greater than 2 are composite
+        for (int i = factorStart; i <= end; i+=2)
+            FactorFinder.findFactors(i, factorMap);
+
+        // All odd numbers greater than 3 may be prime
+        for (int i = primeStart; i <= end; i+=2)
+            if (!PrimeFinder.isPrime(i, primeList))
+                FactorFinder.findFactors(i, factorMap);
+
+        System.out.println("Finished in " + (System.nanoTime() - startTime) + "ns\n\n");
+        //System.out.println(primeList);
+        //System.out.println(factorMap+"\n\n");
+    }
+
+
 }
