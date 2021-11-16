78aca2660594c19bbca586d9ecc7a10650cfce7c
randytbushman <bushmanr1@students.rowan.edu>
2021-11-15 22:04:28 -0500

    Added additional functionality to buttons

--- src/assignment1/FactorizerGUI.java
+++ src/assignment1/FactorizerGUI.java
@@ -2,22 +2,55 @@ package assignment1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FactorizerGUI
 {
+    private int primeTally = 0;
+    private JTextField start;
+    private JTextField end;
+
+
     public FactorizerGUI() {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
+        start = new JTextField();
+        end = new JTextField();
+        JButton submit = new JButton("Submit");
+        JButton cancel = new JButton("Cancel");
+        JLabel tally = new JLabel("Primes computed: ");
 
-        panel.setBorder(BorderFactory.createEmptyBorder(500,500,500,500));
-        panel.setLayout(new GridLayout());
+
+        start.setPreferredSize(new Dimension(100, 30));
+        end.setPreferredSize(new Dimension(100, 30));
+        submit.addActionListener(e -> runPrimeThing());
+        cancel.addActionListener(e -> cancelPrimeThing());
+
+        panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
+        panel.setLayout(new GridLayout(0,2));
+
+        panel.add(new JLabel("Start:"));
+        panel.add(start);
+        panel.add(new JLabel("End:"));
+        panel.add(end);
+        panel.add(submit);
+        panel.add(cancel);
+        panel.add(tally);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
 
+    public void runPrimeThing() {
+        System.out.println(Integer.parseInt(start.getText()));
+        System.out.println(Integer.parseInt(end.getText()));
+    }
 
+    public void cancelPrimeThing() {
+        System.out.println("Canceling");
     }
 }
